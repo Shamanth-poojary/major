b8b00dcf8548843c5da79c9337efb3bd145311f2
shamanth poojary <shamanthpoojary111@gmail.com>
2026-01-16 12:08:29 +0530

    restored misshandled routing codes

--- routes/listing.js
+++ routes/listing.js
@@ -1,3 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const validateListing = require("../utils/validateListing");
+const { isLoggedIn } = require("../middleware.js");
+const { isOwner } = require("../middleware.js");
+const listingController = require("../controller/listings.js");
+router
+  .route("/")
+  .get(wrapAsync(listingController.index)) //index route
+  .post(
+    isLoggedIn, //post route
+    validateListing,
+    wrapAsync(listingController.postListing)
+  );
+//new route
+router.get("/new", isLoggedIn, listingController.newform);
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing)) //show route
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  ); //update route
+
 //edit route
 router.get(
   "/:id/edit",
@@ -5,3 +33,13 @@ router.get(
   isOwner,
   wrapAsync(listingController.editListing)
 );
+
+//delete route
+router.delete(
+  "/:id",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.deleteListing)
+);
+
+module.exports = router;
